6e271839a1183172f42e734c3cd7a1e4ee908ebe
Ajish Dharman <114971015+ajishd-yougotagift@users.noreply.github.com>
2026-03-06 15:16:44 +0530

    4th commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 app = Flask(__name__)
 
 @app.route("/")
@@ -6,4 +7,5 @@ def home():
     return "<html><head><title>Demo App</title></head><body>Demo App Running</body></html>"
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
